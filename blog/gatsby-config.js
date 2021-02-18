035f0524d4dcd345be7a99bc89d0d3d6bc21bb94
Jackie Aldama <jaldama@nuxeo.com>
2021-02-18 17:41:58 -0500

    Gatsby blog

---
+++ blog/gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `dev rantings`,
+    author: `Jackie Sweet`,
+    description: `thoughts and rantings about code and beyond`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/eikkaj`,
+      },
+    ],
+  },
+}
